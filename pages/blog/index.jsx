7facecdb3305fc2078278bf5fc4f7ddbfd39d637
badawi1713 <dzaky.badawi@gmail.com>
2022-01-30 19:01:56 +0700

    Update blog page layout

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Footer, Layout } from "components";
+import { IoCalendar } from 'react-icons/io5'
 import { SVGHello } from 'public/svg'
 import Image from "next/image";
 import moment from "moment";
+
+import Masonry from 'react-masonry-css';
+
+const breakpointColumnsObject = {
+  default: 2,
+  1100: 2,
+  700: 1,
+  500: 1
+};
+
 const Blog = () => {
   return (
     <Layout title={"All Posts"}>
@@ -17,8 +28,9 @@ const Blog = () => {
           </div>
         </header>
 
-        <main className="grid md:grid-cols-4 grid-cols-1 gap-10 px-8 my-8">
-          <section className="flex flex-col md:col-span-3 gap-8">
+        <main className="grid md:grid-cols-6 grid-cols-1 gap-10 px-8 my-8">
+
+          <section className="flex flex-col md:col-span-4 gap-8">
             <div className="bg-white dark:bg-slate-700 rounded-md shadow-md flex flex-col-reverse md:flex-row justify-between md:gap-8">
 
               <div className="py-8 px-4 flex flex-col gap-4">
@@ -31,7 +43,10 @@ const Blog = () => {
               />
             </div>
             <hr />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <Masonry
+              breakpointCols={breakpointColumnsObject}
+              className="my-masonry-grid flex w-full gap-8 "
+              columnClassName="my-masonry-grid_column flex flex-col gap-8">
               <div className="bg-white dark:bg-slate-700 rounded-md shadow-md flex flex-col">
                 <img src="https://media.istockphoto.com/photos/every-time-you-open-a-book-you-learn-something-picture-id1182489122?b=1&k=20&m=1182489122&s=170667a&w=0&h=OUPs_giwFPXLNDzj-9ZZEKswO0ydMvtPKNFph4AX6JE="
                   alt="illustration"
@@ -40,9 +55,13 @@ const Blog = () => {
                 <div className="py-8 px-4 flex flex-col justify-between flex-1 gap-4">
                   <div className="flex flex-col gap-4">
                     <h1 className="text-2xl font-semibold">long established</h1>
-                    <p className="text-base leading-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....</p>
+                    <p className="text-base leading-5">It is a
+                      long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....</p>
                   </div>
-                  <p className="text-right mt-4">{moment(new Date()).format('DD/MM/YYYY HH:mm')}</p>
+                  <div className="flex items-center gap-2 mt-4">
+                    <IoCalendar className="text-red-400 dark:text-sky-400" />
+                    <p>{moment(new Date()).format('DD/MM/YYYY HH:mm')}</p>
+                  </div>
                 </div>
 
               </div>
@@ -56,7 +75,10 @@ const Blog = () => {
                     <h1 className="text-2xl font-semibold">long established</h1>
                     <p className="text-base leading-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....</p>
                   </div>
-                  <p className="text-right mt-4">{moment(new Date()).format('DD/MM/YYYY HH:mm')}</p>
+                  <div className="flex items-center gap-2  mt-4">
+                    <IoCalendar className="text-red-400 dark:text-sky-400" />
+                    <p>{moment(new Date()).format('DD/MM/YYYY HH:mm')}</p>
+                  </div>
                 </div>
 
               </div>
@@ -70,13 +92,17 @@ const Blog = () => {
                     <h1 className="text-2xl font-semibold">long established</h1>
                     <p className="text-base leading-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....</p>
                   </div>
-                  <p className="text-right mt-4">{moment(new Date()).format('DD/MM/YYYY HH:mm')}</p>
+                  <div className="flex items-center gap-2  mt-4">
+                    <IoCalendar className="text-red-400 dark:text-sky-400" />
+                    <p>{moment(new Date()).format('DD/MM/YYYY HH:mm')}</p>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Masonry>
+
           </section>
 
-          <section className="md:sticky md:top-8 px-4 py-8 self-start bg-white dark:bg-slate-700 rounded-md shadow-md flex flex-col gap-8">
+          <section className="md:sticky md:top-8 px-4 md:col-span-2 py-8 self-start bg-white dark:bg-slate-700 rounded-md shadow-md flex flex-col gap-8">
             <h1 className="text-2xl font-semibold">recent posts</h1>
             <div className="flex flex-col gap-4">
               <div className="flex items-center gap-4">
