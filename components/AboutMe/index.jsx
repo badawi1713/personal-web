3bc7e29dff16853c9a1ced55cd212778d1beb445
badawi1713 <dzaky.badawi@gmail.com>
2022-01-31 07:44:04 +0700

    Update download resume text position

--- components/AboutMe/index.jsx
+++ components/AboutMe/index.jsx
@@ -15,7 +15,7 @@ const AboutMe = () => {
                     As a front-end developer I am very interested in topics about
                     web application development using React.js and UI/UX design.
                 </p>
-                <a href="https://bit.ly/3fNmrWc" rel="noreferrer" target={"_blank"} className=" bg-red-400 hover:bg-red-500 dark:bg-sky-400 hover:dark:bg-sky-500 text-white py-4 px-8 rounded-lg hover:-translate-y-1 transition ">Download Resume</a>
+                <a href="https://bit.ly/3fNmrWc" rel="noreferrer" target={"_blank"} className=" bg-red-400 text-center hover:bg-red-500 dark:bg-sky-400 hover:dark:bg-sky-500 text-white py-4 px-8 rounded-lg hover:-translate-y-1 transition ">Download Resume</a>
             </section>
             <section className="w-48 h-48 md:w-72 md:h-72 lg:w-80 lg:h-80 bg-red-400 dark:bg-sky-300 relative rounded-full">
                 <div className="h-44 w-44 md:w-64 md:h-64 lg:w-72 lg:h-72 bg-white rounded-full absolute top-0 right-0">
--- pages/blog/post/[slug].jsx
+++ pages/blog/post/[slug].jsx
@@ -15,7 +15,13 @@ const Post = () => {
           </div>
         </header>
         <article className="p-4 md:p-8 dark:bg-slate-700 rounded-md shadow-md">
-          <p>TEST</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas hendrerit a felis id tempor. Fusce augue sem, tempus in velit non, sagittis pretium justo. Donec lacinia eu ante iaculis egestas. Donec magna lorem, cursus in aliquet in, pellentesque a tortor. Vestibulum eros nunc, tristique tristique congue et, posuere vel felis. Interdum et malesuada fames ac ante ipsum primis in faucibus. In aliquet dolor in tempus pellentesque. Mauris et viverra nulla, ut suscipit metus. Morbi porttitor egestas nulla, vitae ultricies ante luctus vel. Integer iaculis convallis est, vehicula tristique nibh placerat ac. Sed et sollicitudin dui.
+            Aliquam sit amet maximus lorem. Aliquam placerat ligula et nulla vehicula, in ullamcorper diam convallis. Proin eget posuere lacus. Donec posuere vestibulum eros vel pretium. Fusce ut mi vel risus aliquam egestas id eget ligula. Donec malesuada sem ac sem molestie, nec porta sapien consectetur. Etiam vitae massa non est commodo venenatis. Sed vel ex sit amet libero iaculis fringilla. Donec sed nulla accumsan, rhoncus urna ut, accumsan nulla. Aliquam ullamcorper bibendum bibendum.
+            In vitae mi dignissim, molestie lectus quis, pellentesque purus. Nam metus nibh, mattis ut dui ut, interdum facilisis tortor. Nunc eu odio a nisi ullamcorper congue quis vel dui. Praesent blandit porttitor imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc rutrum vel tortor eu iaculis. Quisque id massa gravida, euismod dui ut, iaculis ex. Nulla id libero feugiat dolor condimentum cursus. Nulla hendrerit augue nisi, sed maximus dolor scelerisque eu. Cras vitae fermentum felis, nec interdum ligula.
+            Cras pharetra ipsum ut euismod tincidunt. Mauris id arcu id justo efficitur tempor. Fusce eleifend nisi ac nisi ornare, ac interdum sem euismod. Quisque eu nibh condimentum dolor congue vehicula. Praesent placerat purus in eros egestas, ac placerat nulla placerat. Integer molestie consectetur velit eu molestie. Aenean a urna nunc. Curabitur eu libero cursus, pellentesque neque at, tincidunt ligula.
+            Aenean tincidunt dolor id quam lobortis posuere. Morbi pretium sit amet ex rhoncus pharetra. Vestibulum metus mauris, varius dictum mi sed, sollicitudin fringilla enim. Fusce eget leo odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur imperdiet sem at sapien mattis venenatis. Sed pellentesque nec massa nec molestie. Vivamus ac libero id arcu bibendum ultrices. Nunc nec ex ut nunc dapibus vestibulum vel vel velit. Sed at gravida lacus, non rhoncus erat.
+          </p>
         </article>
         <Author />
       </div>
